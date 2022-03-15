b16628f5b4dc1e24c35a397817984f4b40482ef7
Jo <joel.zhuo.cheng@gmail.com>
2022-03-15 18:04:28 +0900

    file movement

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./style/index.less";
 import Food from "./modules/Food";
-import ScorePanel from "./ScorePanel";
+import ScorePanel from "./modules/ScorePanel";
 
 const food = new Food();
 console.log(food.X,food.Y);
